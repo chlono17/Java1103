b2080950e7eb4939aadb9789d3590e56f8be4ada
https://github.com/chlono17/Java1103/ <>
2020-11-03 22:33:46 +0800

    java

--- src/main/java/com/ocp/PrintStars.java
+++ src/main/java/com/ocp/PrintStars.java
@@ -1,25 +1,27 @@
 package com.ocp;
 
 public class PrintStars {
+
     public static void main(String[] args) {
-       int line = 7; 
+        int line = 7;
         /*利用for
         print:  *
                 **
                 ***
                 ****
                 *****
-        */
-        
-        
-        for (int i =1; i <= line; i++) {
-            for (int j = 1; j <=i; j++) {
-                System.out.print("*");
-               
+         */
+
+        for (int i = 1; i <= line; i++) {
+            for (int j = 6; j >= i; j--) {
+                System.out.print(" ");
+
             }
-            
+            for (int x = 1; x < i; x++) {
+                System.out.print("*");
+            }
             System.out.println("");
         }
     }
-    
+
 }
