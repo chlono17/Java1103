33b2312c52ef550c9f7a9a0b9c4db70783ed2281
MB-study <MB-study@192.168.1.113>
2020-11-05 21:42:12 +0800

    Java

--- src/main/java/com/ocp/PrintStars.java
+++ src/main/java/com/ocp/PrintStars.java
@@ -20,12 +20,33 @@ public class PrintStars {
         }
 
         //homework
-        for (int i = 1; i <= line; i++) {
-            for (int j = 6; j >= i; j--) {
+        for (int i = 1; i <= line+1; i++) {
+            for (int j = line; j >= i; j--) {
                 System.out.print(" ");
-
             }
             for (int x = 1; x < i; x++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        
+        
+
+        //pro ver
+        /*
+        print:
+        *
+        **
+        ***
+        ****
+        ***
+        **
+        *
+        
+        */
+        int m = (int)(Math.ceil(line/2.0));
+        for (int i = 1; i < line; i++) {
+            for (int j = 1; j < m; j++) {
                 System.out.print("*");
             }
             System.out.println("");
