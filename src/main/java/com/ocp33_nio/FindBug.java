862f5a53eb1b64a0ead397c649ed456a91672fb4
MB-study <MB-study@DESKTOP-KVQTFGR>
2021-02-18 20:07:05 +0800

    Java

---
+++ src/main/java/com/ocp33_nio/FindBug.java
@@ -0,0 +1,21 @@
+package com.ocp33_nio;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+public class FindBug {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("src\\main\\java\\com\\ocp33_nio\\score.csv");
+        Optional<String> opt = Files.readAllLines(path)
+                .stream()
+                .parallel()
+                .peek(s -> System.out.println(s))
+                .filter(s -> s.contains("bug"))
+                .findFirst();
+        System.out.println(opt.isPresent());
+    }
+    
+}
---
+++ src/main/java/com/ocp33_nio/ReadAllLinesDemo.java
@@ -0,0 +1,39 @@
+package com.ocp33_nio;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ReadAllLinesDemo {
+    public static void main(String[] args) {
+        Path path = Paths.get("src\\main\\java\\com\\ocp33_nio\\score.csv");
+        // 1. BufferedRead + FileReader
+        try (FileReader fr = new FileReader(path.toFile());
+                BufferedReader br = new BufferedReader(fr);) {
+            br.lines().forEach(System.out::println);
+        } catch (Exception e) {
+        }
+        System.out.println("");
+        // 2. Files.newBufferedReader
+        try (BufferedReader br = Files.newBufferedReader(path)){
+            br.lines().forEach(System.out::println);
+        } catch (Exception e) {
+        }
+        System.out.println("");
+        // 3. Files.readAllLines(會得到一個 List 集合)
+        try {
+            List<String> list = Files.readAllLines(path);
+            list.forEach(System.out::println);
+        } catch (Exception e) {
+        }
+        System.out.println("");
+        // 4. Files.lines
+        try {
+            Files.lines(path).forEach(System.out::println);
+        } catch (Exception e) {
+        }
+    }
+}
